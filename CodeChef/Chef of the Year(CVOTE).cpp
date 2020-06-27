e89e240e87e7e9586d22f878a667de99bc562828
kushgupta-official <kushgupta.potus@gmail.com>
2020-06-28 04:07:15 +0530

    CVOTE

--- CodeChef/Chef of the Year(CVOTE).cpp
+++ CodeChef/Chef of the Year(CVOTE).cpp
@@ -7,7 +7,11 @@ using namespace std;
 # define vll std::vector<ll>
 
 int main()
-{
+{	
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
 	ll n=0,m=0;
 	cin>>n>>m;
 	std::map<string, string> entries;
@@ -36,7 +40,7 @@ int main()
 				max_chef=chef;
 			}
 		}
-		
+
 		vote_countries[country]++;
 		if (vote_countries[country]>max_co){
 			max_co=vote_countries[country];
