4bab2bd69ce096cfc7c3929885b0cc2efd58a8ab
kushgupta-official <kushgupta.potus@gmail.com>
2020-07-03 01:37:33 +0530

    HMAPPY2

--- CodeChef/Appy and Contest(HMAPPY2).cpp
+++ CodeChef/Appy and Contest(HMAPPY2).cpp
@@ -5,6 +5,7 @@ using namespace std;
 # define rep(i,a,b) for(ll i=a;i<b;i++)
 # define vi std::vector<int>
 # define vll std::vector<ll>
+# define w(x) ll x=0;cin>>x;while(x--)
 
 void kush_gupta(){
   ios::sync_with_stdio(0);
@@ -15,10 +16,28 @@ void kush_gupta(){
   freopen("output.txt", "w", stdout) ;
   #endif
 }
-
+ll find_gcd(ll a,ll b){
+  ll temp=0;
+  while(b!=0){
+    temp=b;
+    b=a%b;
+    a=temp;
+  }
+  return a;
+}
 int main()
 {
-	kush_gupta();
-	
+	  kush_gupta();
+	  w(test_cases){
+    ll n=0,a=0,b=0,k=0;
+    cin>>n>>a>>b>>k;
+    ll lcm=(a*b)/find_gcd(a,b);
+    if (n/a + n/b - n/lcm -n/lcm >= k){
+      cout<<"Win\n";
+    }
+    else{
+      cout<<"Lose\n";
+    }
+  }
 	return 0;
 }
