76249845e37862cdfdc19f4977585d732ec80c34
kushgupta-official <kushgupta.potus@gmail.com>
2020-07-09 04:30:06 +0530

    QHOUSE

--- CodeChef/DSA/Bear and House Queries(QHOUSE).cpp
+++ CodeChef/DSA/Bear and House Queries(QHOUSE).cpp
@@ -10,10 +10,10 @@ void kush_gupta(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  /*#ifndef ONLINE_JUDGE
+  #ifndef ONLINE_JUDGE
   freopen("input.txt", "r", stdin) ;
   freopen("output.txt", "w", stdout) ;
-  #endif*/
+  #endif
 }
 
 ll bs(ll a, ll b, ll y, int cord){
