de14854e3f34d20d1bfa953e349ca3688204c707
kushgupta-official <kushgupta.potus@gmail.com>
2020-09-03 02:17:01 +0530

    POLYREL

---
+++ CodeChef/August Cook-Off 2020 Division 2/Polygon Relationship (POLYREL).cpp
@@ -0,0 +1,45 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+# define ll long long
+# define ld long double
+# define rep(i,a,b) for(ll i=a;i<b;i++)
+# define vi std::vector<int>
+# define vll std::vector<ll>
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin) ;
+  freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+int main()
+{
+	kush_gupta();
+	w(tset_cases){
+    ll n=0;
+    cin>>n;
+    ll x=0,y=0;
+    rep(i,0,n){
+      cin>>x>>y;
+    }
+    ll sum=0;
+    sum+=n;
+    while(n>5){
+      n/=2;
+      sum+=n;
+    }
+    cout<<sum<<endl;
+  }
+	return 0;
+}
