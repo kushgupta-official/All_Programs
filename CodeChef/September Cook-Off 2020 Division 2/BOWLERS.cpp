157a56be5b9754ab552c5e393273f3a3cadf0da6
kushgupta-official <kushgupta.potus@gmail.com>
2020-09-25 12:42:06 +0530

    Day of the programmer

--- CodeChef/September Cook-Off 2020 Division 2/BOWLERS.cpp
+++ CodeChef/September Cook-Off 2020 Division 2/BOWLERS.cpp
@@ -17,10 +17,10 @@ void kush_gupta(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  #ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin) ;
-  freopen("output.txt", "w", stdout) ;
-  #endif
+  // #ifndef ONLINE_JUDGE
+  // freopen("input.txt", "r", stdin) ;
+  // freopen("output.txt", "w", stdout) ;
+  // #endif
 }
 
 int main()
