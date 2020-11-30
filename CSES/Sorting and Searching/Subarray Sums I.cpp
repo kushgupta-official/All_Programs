6711fec46f63626c1dbd3ec0baac7bc9304105a3
kushgupta-official <kushgupta.potus@gmail.com>
2020-12-01 02:24:29 +0530

    Subarrays Sums 1 (CSES)

--- CSES/Sorting and Searching/Subarray Sums I.cpp
+++ CSES/Sorting and Searching/Subarray Sums I.cpp
@@ -14,6 +14,24 @@ void kush_gupta(){
 
 int main(){
 	kush_gupta();
-	
+	ll n=0,x=0;
+	cin>>n>>x;
+	vector <ll> a(n);
+	for (ll i=0;i<n;i++){
+		cin>>a[i];
+	}
+	ll currsum=0,count=0;
+	unordered_map <ll,ll> ump;
+	for (ll i=0;i<n;i++){
+		currsum+=a[i];
+		if (currsum==x){
+			count++;
+		}
+		if (ump.find(currsum-x)!=ump.end()){
+			count+=ump[currsum-x];
+		}
+		ump[currsum]++;
+	}
+	cout<<count;
 	return 0;
 }
