6e56109f5b4f486f44131a5729bd089401433fce
kushgupta-official <kushgupta.potus@gmail.com>
2020-12-04 02:54:05 +0530

    Money Sums (CSES)

--- CSES/Dynamic Programming/Money Sums.cpp
+++ CSES/Dynamic Programming/Money Sums.cpp
@@ -1,5 +1,5 @@
 # include <bits/stdc++.h>
-
+# define ll long long int
 using namespace std;
 
 void kush_gupta(){
@@ -12,13 +12,6 @@ void kush_gupta(){
 	#endif
 }
 
-int dp(int n,vector <int> &a){
-	if (n==0){
-		return 0;
-	}
-	return 1+dp(n-1,a);
-}
-
 int main(){
 	kush_gupta();
 	int n=0;
@@ -27,6 +20,21 @@ int main(){
 	for (int i=0;i<n;i++){
 		cin>>a[i];
 	}
-	cout<<dp(n,a);
+	set <int> s;
+	s.insert(a[0]);
+	for (ll i=1;i<n;i++){
+		vector <ll> v;
+		for (auto ite:s){
+			v.push_back(a[i]+ite);
+		}
+		s.insert(a[i]);
+		for (auto ite:v){
+			s.insert(ite);
+		}
+	}
+	cout<<s.size()<<endl;
+	for (auto ite:s){
+		cout<<ite<<" ";
+	}
 	return 0;
 }
