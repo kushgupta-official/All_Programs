d16bd86cba986c3ddcf2336f3b1f6535dfebb16e
kushgupta-official <kushgupta.potus@gmail.com>
2020-12-17 00:32:47 +0530

    Building Roads

--- CSES/Graph Algorithms/Building Roads.cpp
+++ CSES/Graph Algorithms/Building Roads.cpp
@@ -1,5 +1,5 @@
 # include <bits/stdc++.h>
-
+ 
 using namespace std;
 # define ll long long
 # define ld long double
@@ -16,28 +16,56 @@ using namespace std;
 # define mid(l,r) l+(r-l)/2
 # define fi first
 # define se second
-
+ 
 void kush_gupta(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
   #ifndef ONLINE_JUDGE
-  	freopen("input.txt", "r", stdin) ;
-  	freopen("output.txt", "w", stdout) ;
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
   #endif
 }
-
+ 
+void dfs(vvl &v, vector <bool> &visited, ll i){
+  if (visited[i]){
+    return;
+  }
+  visited[i]=true;
+  for (auto j:v[i]){
+    dfs(v, visited, j);
+  }
+}
+ 
+void solve(vvl &v, ll n){
+  vector <bool> visited(n+1,false);
+  vector <pair <ll,ll> > result;
+  ll prev=0;
+  dfs(v, visited, 1);
+  for (ll i=2;i<=n;i++){
+    if (!visited[i]){
+      dfs(v, visited, i);
+      result.push_back({i-1,i});
+    }
+  }
+  cout<<result.size()<<endl;
+  for (ll i=0;i<result.size();i++){
+    cout<<result[i].fi<<" "<<result[i].se<<endl;
+  }
+}
+ 
 int main()
 {
-	kush_gupta();
-	ll n=0,m=0;
-	cin>>n>>m;
-	vvl v(n+1);
-	for (ll i=1;i<=m;i++){
-		ll a=0,b=0;
-		cin>>a>>b;
-		v[a].push_back(b);
-	}
-	
-	return 0;
-}
+  kush_gupta();
+  ll n=0,m=0;
+  cin>>n>>m;
+  vvl v(n+1);
+  for (ll i=1;i<=m;i++){
+    ll a=0,b=0;
+    cin>>a>>b;
+    v[a].push_back(b);
+    v[b].push_back(a);
+  }
+  solve(v,n);
+  return 0;
+} 
