0093696ad9053afd96b095342d9ad624766075bd
kushgupta-official <kushgupta.potus@gmail.com>
2020-12-22 02:13:25 +0530

    CATHIEF (CodeChef)

---
+++ CodeChef/December Cook-Off 2020 Division 2/CATHIEF.cpp
@@ -0,0 +1,64 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin) ;
+  	freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+int main()
+{
+	kush_gupta();
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+	#endif
+
+	w(t){
+		ll x=0, y=0, k=0, n=0;
+		cin>>x>>y>>k>>n;
+		if (x>y){
+			swap(x,y);
+		}
+		while (y>=0 && x<=n){
+			x+=k;
+			y-=k;
+			if (x==y){
+				cout<<"Yes\n";
+				break;
+			}
+		}
+		if (x!=y){
+			cout<<"No\n";
+		}
+	}
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+	return 0;
+}
