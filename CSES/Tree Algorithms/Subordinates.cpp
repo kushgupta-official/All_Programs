ad25136510581655f394a7ec953fb29d10cbc3a3
kushgupta-official <kushgupta.potus@gmail.com>
2021-01-02 03:29:50 +0530

    Subordinates (CSES)

---
+++ CSES/Tree Algorithms/Subordinates.cpp
@@ -0,0 +1,78 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin) ;
+  	freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+vector <vector <ll> > tree;
+ll n;
+vector <ll> subs;
+
+void solve(ll node, ll parent){
+	ll subords=0;
+	for (auto child:tree[node]){
+		if (child!=parent){
+			solve(child, node);
+			subords+=1+subs[child];
+		}
+	}
+	subs[node]=subords;
+}
+
+int main()
+{
+	kush_gupta();
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+	#endif
+
+	cin>>n;
+
+	tree.resize(n+1);
+	subs.resize(n+1);
+
+	loop(i,2,n+1){
+		ll x=0;
+		cin>>x;
+		tree[i].push_back(x);
+		tree[x].push_back(i);
+	}
+
+	solve(1,0);
+
+	loop(i,1,n+1){
+		cout<<subs[i]<<" ";
+	}
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+	return 0;
+}
