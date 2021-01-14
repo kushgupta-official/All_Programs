52caf3f92307858f8a3f40358da1b0af2b5e80f6
kushgupta-official <kushgupta.potus@gmail.com>
2021-01-15 01:58:38 +0530

    String LCM - cf

---
+++ CodeForces/Educational Codeforces Round 102 (Rated for Div. 2)/B - String LCM.cpp
@@ -0,0 +1,66 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin) ;
+  	freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+string operator*(const string& s, unsigned int n) {
+    string temp;
+    while (n--)
+        temp+=s;
+    return temp;
+}
+
+int main()
+{
+	kush_gupta();
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+	#endif
+
+	w(q){
+		string s,t;
+		cin>>s>>t;
+		ll lcm=(s.size()*t.size()) / __gcd(s.size(), t.size());
+		s=s*(lcm/s.size());
+		t=t*(lcm/t.size());
+		if (s==t){
+			cout<<s<<endl;
+		}
+		else{
+			cout<<-1<<endl;
+		}
+	}	
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+	return 0;
+}
