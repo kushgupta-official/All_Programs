e529ae14fa6902c2c1b91383070b92ded6b7f484
kushgupta-official <kushgupta.potus@gmail.com>
2021-03-10 00:03:00 +0530

    Josephus Problem II (CSES)

---
+++ CSES/Sorting and Searching/Josephus Problem II.cpp
@@ -0,0 +1,65 @@
+# include <bits/stdc++.h>
+# include <ext/pb_ds/assoc_container.hpp> 
+# include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace __gnu_pbds; 
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin) ;
+  	freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+int main()
+{
+	kush_gupta();
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+	#endif
+
+	ordered_set oset;
+	ll n=0,k=0;
+	cin>>n>>k;
+	for (ll i=1;i<=n;i++){
+		oset.insert(i);
+	}
+	ll curr=k%n;
+	while(oset.size()){
+		ll r=*oset.find_by_order(curr);
+		cout<<r<<" ";
+		oset.erase(r);
+		if (oset.size()){
+			curr=(curr+k)%oset.size();
+		}
+	}
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+	return 0;
+}
