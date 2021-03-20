8ce74802f68c20187a2694649afd57e2daee6771
kushgupta-official <kushgupta.potus@gmail.com>
2021-03-20 14:54:21 +0530

    Minimal Cost (CodeForces)

---
+++ CodeForces/Codeforces Global Round 13/Minimal Cost.cpp
@@ -0,0 +1,77 @@
+# include <bits/stdc++.h>
+# include <ext/pb_ds/assoc_container.hpp> 
+# include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+
+void kush_gupta_solves(){
+	w(t){
+		ll n,u,v;
+		cin>>n>>u>>v;
+		vll arr(n);
+		cin>>arr[0];
+		bool diag=0,noCost=0;
+		loop(i,1,n){
+			cin>>arr[i];
+			if (abs(arr[i]-arr[i-1])==1){
+				diag=1;
+			}
+			else if (arr[i]!=arr[i-1]){
+				noCost=1;
+			}
+		}
+		if (noCost){
+			cout<<0<<endl;
+		}
+		else if (diag){
+			cout<<min(u,v)<<endl;
+		}
+		else{
+			cout<<min(u+v,v+v)<<endl;
+		}
+	}
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+		freopen("input.txt", "r", stdin) ;
+  		freopen("output.txt", "w", stdout) ;
+	#endif
+
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	kush_gupta_solves();
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+
+	return 0;
+}
