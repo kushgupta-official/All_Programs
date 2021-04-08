894c5905bd2fde6bbe97367ea9ad7dfab5f2c70e
kushgupta-official <kushgupta.potus@gmail.com>
2021-04-09 02:52:43 +0530

    Test

--- CSES/Sorting and Searching/test.cpp
+++ CSES/Sorting and Searching/test.cpp
@@ -16,7 +16,7 @@ using namespace __gnu_pbds;
 # define pp pair <pair <ll,ll>,ll>
 # define pb(x) push_back(x)
 # define w(x) ll x=0; cin>>x; while(x--)
-# define endl '\n'
+// # define endl '\n'
 # define mid(l,r) l+(r-l)/2
 # define fi first
 # define se second
@@ -24,55 +24,81 @@ using namespace __gnu_pbds;
 template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
 
 void kush_gupta_solves(){
-	w(t){
-		ll n=0;
-		cin>>n;
-		string s;
-		cin>>s;
-		string a,b;
-		if (s[0]=='0' or s[n-1]=='0'){
-			cout<<"NO\n";
-			continue;
-		}
-		vll ones,zeroes;
-		loop(i,1,n-1){
-			if (s[i]=='1')
-				ones.push_back(i);
-			else
-				zeroes.push_back(i);
-		}
-		if (ones.size()%2 or zeroes.size()%2){
-			cout<<"NO\n";
-			continue;
-		}
-		a=s;
-		b=s;
-		a[0]='('; a[n-1]=')';
-		b[0]='('; b[n-1]=')';
-		bool alter=0;
-		loop(i,0,zeroes.size()){
-			if (alter==0){
-				a[zeroes[i]]='(' ;
-				b[zeroes[i]]=')' ;
+	ll n=0;
+	cin>>n;
+	ll bi=1,bj=-1;
+	ll wi=1,wj=0;
+	ll temp=0;
+	loop(k,0,n*n){
+		cin>>temp;
+		if (temp==1){
+			if (bi+1>n and bj+2>n){
+				if (wj+2>n){
+					wi+=1;
+					wj= wj+2==n+1 ? 2 : 1;
+				}
+				else{
+					wj+=2;
+				}
+				cout<<3<<" "<<wi<<" "<<wj<<endl;
 			}
 			else{
-			   b[zeroes[i]]='(' ;
-			   a[zeroes[i]]=')' ;
+				if (bj+2>n){
+					bi+=1;
+					// if (bj+2==n+1)
+					// 	bj=1;
+					bj= bj+2==n+1 ? 2 : 1;
+				}
+				else{
+					bj+=2;
+				}
+				cout<<2<<" "<<bi<<" "<<bj<<endl;
 			}
-			alter=!alter;
 		}
-		loop(i,0,ones.size()/2){
-			a[ones[i]]='(';
-			b[ones[i]]='(';
+		else if (temp==2){
+			if (wi+1>n and wj+2>n){
+				if (bj+2>n){
+					bi+=1;
+					bj= bj+2==n+1 ? 2 : 1;
+				}
+				else{
+					bj+=2;
+				}
+				cout<<3<<" "<<bi<<" "<<bj<<endl;
+			}
+			else{
+				if (wj+2>n){
+					wi+=1;
+					wj= wj+2==n+1 ? 2 : 1;
+				}
+				else{
+					wj+=2;
+				}
+				cout<<1<<" "<<wi<<" "<<wj<<endl;
+			}
 		}
-		loop(i,ones.size()/2,ones.size()){
-			a[ones[i]]=')';
-			b[ones[i]]=')';
+		else{
+			if (wj+2<=n or wi+1<=n){
+				if (wj+2>n){
+					wi+=1;
+					wj= wj+2==n+1 ? 2 : 1;
+				}
+				else{
+					wj+=2;
+				}
+				cout<<1<<" "<<wi<<" "<<wj<<endl;
+			}
+			else{
+				if (bj+2>n){
+					bi+=1;
+					bj= bj+2==n+1 ? 2 : 1;
+				}
+				else{
+					bj+=2;
+				}
+				cout<<2<<" "<<bi<<" "<<bj<<endl;
+			}
 		}
-		cout<<"YES\n";
-		cout<<a<<endl;
-		cout<<b<<endl;
-		
 	}
 }
 
@@ -84,9 +110,9 @@ int main()
   		freopen("output.txt", "w", stdout) ;
 	#endif
 
-	ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	// ios::sync_with_stdio(0);
+ //    cin.tie(0);
+ //    cout.tie(0);
 
 	kush_gupta_solves();
 
