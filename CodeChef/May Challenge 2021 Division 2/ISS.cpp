91801c23ed18a16c13ed3dcec07e89c9f2952bd2
kushgupta-official <kushgupta.potus@gmail.com>
2021-05-18 21:05:36 +0530

    ISS--CodeChef

---
+++ CodeChef/May Challenge 2021 Division 2/ISS.cpp
@@ -0,0 +1,84 @@
+# include <bits/stdc++.h>
+# include <ext/pb_ds/assoc_container.hpp> 
+# include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+
+void kush_gupta_solves(){
+	int n=4000009;
+	vi v(n);
+	vi res(n,0);
+	//int tc=0;
+    for(int i=0;i<n;i++) {
+        v[i] = i;
+    }
+    for(int i=2;i<n;i++) {
+    	//tc++;
+        if(v[i]==i) {
+            v[i]=i-1;
+            for(int j=2*i;j<n;j+=i) {
+            	//tc++;
+                int x=v[j]/i;
+                v[j]=x*(i-1);
+            }
+        }
+    }
+    for(int i=1;i<n;++i) {
+    	//tc++;
+        res[i]+=i-1;
+        for(int j=2*i;j<n;j+=i) {
+        	// tc++;
+            res[j]+=i*((1+v[j/i])/2);
+        }
+    }
+    // cout<<tc<<endl;
+    w(t){
+        int k ;
+        cin>>k;
+        cout<<res[4*k+1]<<endl;
+    }
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+		freopen("input.txt", "r", stdin) ;
+  		freopen("output.txt", "w", stdout) ;
+	#endif
+
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	kush_gupta_solves();
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+
+	return 0;
+}
