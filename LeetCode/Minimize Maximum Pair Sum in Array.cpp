cc586678e1c6389efa54aff03d74db8924708d49
kushgupta-official <kushgupta.potus@gmail.com>
2021-06-08 04:08:12 +0530

    Minimize Maximum Pair Sum in Array -- LeetCode

---
+++ LeetCode/Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int res=0;
+        for (int i=0;i<n/2;i++){
+            res=max(nums[i]+nums[n-i-1],res);
+        }
+        return res;
+    }
+};
