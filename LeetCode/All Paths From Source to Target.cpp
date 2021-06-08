3ee1075329bfc451ac50869b5ce804640e98a525
kushgupta-official <kushgupta.potus@gmail.com>
2021-06-08 11:22:48 +0530

    All Paths From Source to Target -- LeetCode

---
+++ LeetCode/All Paths From Source to Target.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int target;
+    vector <vector <int> > res;
+    vector <int> temp;
+    
+    void dfs(vector<vector<int>>& graph,int curr=0){
+        temp.push_back(curr);
+        if (curr==target)
+            res.push_back(temp);
+        else{
+            for (auto i:graph[curr]){
+                dfs(graph,i);
+            }
+        }
+        temp.pop_back();
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        target=graph.size()-1;
+        dfs(graph);
+        return res;
+    }
+};
