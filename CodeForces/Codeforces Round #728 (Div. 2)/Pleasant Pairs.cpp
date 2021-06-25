8ed5f022543e3603f4d0d4625d6f05d02119a9fb
kushgupta-official <kushgupta.potus@gmail.com>
2021-06-26 04:32:30 +0530

    Pleasant Pairs -- CodeForces

---
+++ CodeForces/Codeforces Round #728 (Div. 2)/Pleasant Pairs.cpp
@@ -0,0 +1,75 @@
+# include <bits/stdc++.h>
+# include <ext/pb_ds/assoc_container.hpp> 
+# include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+
+void kush_gupta_solves(){
+	w(t){
+		ll n;
+		cin>>n;
+		vll v(n);
+		vll idx(2*n+1,0);
+		loop(i,0,n){
+			cin>>v[i];
+			idx[v[i]]=i+1;
+		}
+		ll res=0;
+		for (ll i=3;i<=2*n-1;i++){
+			for (ll j=1;j*j<i;j++){
+				if (i%j==0 and 
+					idx[j]!=0 and 
+					idx[i/j]!=0 and 
+					idx[j]+idx[i/j]==i)
+				{
+					res++;
+				}
+			}
+		}
+		cout<<res<<endl;
+	}
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		auto __start = chrono::high_resolution_clock::now(); 
+		freopen("input.txt", "r", stdin) ;
+  		freopen("output.txt", "w", stdout) ;
+	#endif
+
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	kush_gupta_solves();
+
+	#ifndef ONLINE_JUDGE
+		auto __end = chrono::high_resolution_clock::now(); 
+		double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+		__time_taken *= 1e-9; 
+		cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+		cout << " sec" << endl;
+	#endif
+
+	return 0;
+}
