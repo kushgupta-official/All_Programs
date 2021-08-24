9537b22fc2efe7518c89af5990ca7fdae5008f61
kushgupta-official <kushgupta.potus@gmail.com>
2021-08-25 04:01:30 +0530

    A - Simply Strange Sort	-- CodeForces

---
+++ CodeForces/Codeforces Round #740 (Div. 2, based on VK Cup 2021 - Final (Engine))/A - Simply Strange Sort.cpp
@@ -0,0 +1,77 @@
+# include <bits/stdc++.h>
+# include <ext/pb_ds/assoc_container.hpp> 
+# include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+# define ll long long
+# define ld long double
+# define loop(i,a,b) for(ll i=a;i<b;i++)
+# define vi vector<int>
+# define vll vector<ll>
+# define vvl vector<vector <ll> >
+# define vp vector <pair <ll,ll> >
+# define vpp vector <pair <pair <ll,ll>,ll> >
+# define pp pair <pair <ll,ll>,ll>
+# define pb(x) push_back(x)
+# define w(x) ll x=0; cin>>x; while(x--)
+# define endl '\n'
+# define mid(l,r) l+(r-l)/2
+# define fi first
+# define se second
+
+template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+
+void kush_gupta_solves(){
+    w(t){
+        ll n;
+        cin>>n;
+        vll v(n+1);
+        loop(i,1,n+1){
+            cin>>v[i];
+        }
+        ll iterations=0,preIt=0;
+        ll i,k=0;
+        for (i=0;i<n;i++){
+            bool flag=0;
+            k++;
+            for (ll j=k%2?1:2;j<=n;j+=2){
+                if (j+1<=n and v[j]>v[j+1]){
+                    flag=1;
+                    swap(v[j],v[j+1]);
+                }
+            }
+            iterations++;
+            if (flag)
+                preIt=iterations;
+            
+        }
+        cout<<preIt<<endl;
+    }
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        auto __start = chrono::high_resolution_clock::now(); 
+        freopen("input.txt", "r", stdin) ;
+          freopen("output.txt", "w", stdout) ;
+    #endif
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    kush_gupta_solves();
+
+    #ifndef ONLINE_JUDGE
+        auto __end = chrono::high_resolution_clock::now(); 
+        double __time_taken=chrono::duration_cast<chrono::nanoseconds>(__end - __start).count(); 
+        __time_taken *= 1e-9; 
+        cout<<"\nTime Taken : "<<fixed<< __time_taken << setprecision(9); 
+        cout << " sec" << endl;
+    #endif
+
+    return 0;
+}
