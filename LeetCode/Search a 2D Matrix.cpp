96b3a6b15297665f6bdca72004753156e1ff9c93
kushgupta-official <kushgupta.potus@gmail.com>
2021-08-28 02:25:07 +0530

    Search a 2D Matrix -- LeetCode

--- LeetCode/Search a 2D Matrix.cpp
+++ LeetCode/Search a 2D Matrix.cpp
@@ -3,17 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int i=0,j=m-1;
-        while(i>=0 and i<n and j<m and j>=0){
-            // cout<<i<<" "<<j<<endl;
-            if (matrix[i][j]==target){
+        int lo=0,hi=n*m-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if (matrix[mid/m][mid%m]==target){
                 return true;
             }
-            else if (target<matrix[i][j]){
-                j--;
+            else if (matrix[mid/m][mid%m]<target){
+                lo=mid+1;
             }
             else{
-                i++;
+                hi=mid-1;
             }
         }
         return false;
